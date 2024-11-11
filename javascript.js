c5a4b2ce2de4e62ede8cdd219cd435b62d7eaa83
cjl-1228 <zaq5891097@gmail.com>
2024-11-11 21:58:19 +0800

    test

---
+++ javascript.js
@@ -0,0 +1,38 @@
+let flippedCards = [];
+let lockBoard = false;
+
+function flipCard(cardElement) {
+    if (lockBoard) return;
+    if (cardElement.classList.contains('flipped')) return;
+
+    cardElement.classList.add('flipped');
+    flippedCards.push(cardElement);
+
+    if (flippedCards.length === 2) {
+        checkForMatch();
+    }
+}
+
+function checkForMatch() {
+    lockBoard = true;
+    const [card1, card2] = flippedCards;
+
+    if (card1.getAttribute('data-card') === card2.getAttribute('data-card')) {
+        setTimeout(() => {
+            alert("配對成功！");
+            resetBoard(true);
+        }, 500);
+    } else {
+        setTimeout(() => {
+            alert("配對失敗！");
+            card1.classList.remove('flipped');
+            card2.classList.remove('flipped');
+            resetBoard(false);
+        }, 1000);
+    }
+}
+
+function resetBoard(isMatch) {
+    flippedCards = [];
+    lockBoard = false;
+}
